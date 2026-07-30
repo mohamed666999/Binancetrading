644459fcf1624b8cf405022bcf7309775115cbe6
mohamed666999 <nimomido07@gmail.com>
2026-07-30 07:56:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -394,7 +394,6 @@ class Config:
         binance_api_key: str = "IX7kLH0ssWHP5TpYMUGcp0pzq4LX4Lqi7m4XtlqMkkq6DCZAsLhoeYZ3533jJFF4"
     binance_secret: str = "LmICnpSpMxL1riv4RfIf0HBGRfhDTP5JhDUYdlPSukpqV7kDTonrZ0j3DWp1a7hU"
     nvidia_api_key: str = "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"
-
     dry_run: bool = True
     leverage: int = 100
     risk_per_trade_pct: float = 500
